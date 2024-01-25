e2c22c30d8ae189f7afdb59013ab499fbbe435c8
madisonnoyer54 <115881298+madisonnoyer54@users.noreply.github.com>
2024-01-25 21:56:35 +0100

    Update main.py
    Q3 ok

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 import secrets
+from sympy import symbols, Eq, solve
 
 #random_bits = secrets.randbits(n)
+
+
+# Question 3
+def Decomp(n):
+    # Décomposer n - 1 en 2^s * d
+    d = n - 1
+    s = 0 # Donc 2^s = 1
+
+    # On divise par 2 
+    while d % 2 == 0:
+        s += 1
+        d //= 2
+
+    return s, d
+
+# Test sur 10000 valeurs différentes
+test_values = range(2, 10002)  # Commence à partir de 2 car la décomposition n'est pas définie pour n=1
+results = []
+
+for n in test_values:
+    s, d = Decomp(n)
+   # print(n-1, '=',2**s*d)
+    if(n-1 != 2**s*d):
+        print("Le teste à echoué")
+
+
+
 
