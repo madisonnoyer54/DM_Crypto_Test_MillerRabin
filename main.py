8d619bff0d99b46f8e8c49cd8b1e6e1f02559377
madisonnoyer54 <115881298+madisonnoyer54@users.noreply.github.com>
2024-01-28 14:30:09 +0100

    Q4 OK

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import secrets
 from sympy import symbols, Eq, solve
+import random
 
 #random_bits = secrets.randbits(n)
 
@@ -12,21 +13,60 @@ def Decomp(n):
 
     # On divise par 2 
     while d % 2 == 0:
-        s += 1
-        d //= 2
+        s += 1 # On compte combien de fois on divise d
+        d //= 2 # On divise d
 
     return s, d
 
-# Test sur 10000 valeurs différentes
+# Test de Decomp sur 10000 valeurs différentes
 test_values = range(2, 10002)  # Commence à partir de 2 car la décomposition n'est pas définie pour n=1
-results = []
 
 for n in test_values:
     s, d = Decomp(n)
-   # print(n-1, '=',2**s*d)
     if(n-1 != 2**s*d):
-        print("Le teste à echoué")
+        print("Le teste à echoué : n - 1 = 2^s * d")
+
+"""
+Cette m´ethode rapide d’exponentiation modulaire est d´ecrite dans l’Annexe A. Il s’agit donc d’impl´e-
+menter ici cette m´ethode sous la forme d’une fonction ExpMod() qui prend en entr´ee n, a et t et qui
+renvoie en sortie at mod n.
+
+Question 4. Impl´ementez la fonction ExpMod(). Testez-la sur 10000 valeurs diff´erentes.
 
 
+
+"""
+
+# Question 4 
+def ExpMod(n, a, t):
+    # Mettre t en binaire 
+    t_binaire = bin(t)[2:]
+
+    # Prendre la taille 
+    r = len(t_binaire)
+
+    # t = 1
+    if t_binaire == '1':
+        return a % n
+
+    # t est pair
+    elif t_binaire[-1] == '0':
+        return ExpMod(n, (a**2) % n, t // 2)
+      
+    # t est impair
+    else: 
+        return (a * ExpMod(n, (a**2) % n, (int(t) - 1) // 2)) % n
+
+
+# Test sur 10000 valeurs différentes
+test_values = range(1, 10001) 
+for z in test_values:
+    a =  random.randint(1, 500)
+    t =  random.randint(1, 500)
+    n =  random.randint(1, 500)
+    f = ExpMod(n,a,t)
+    if(f != a**t % n):
+        print("Le teste a échouer")
+  
 
 
